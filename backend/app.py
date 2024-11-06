95babc35de2470682580349f539fbb0ba49f180d
YuraStasiv <yurastasiv@hotmail.com>
2024-11-06 23:32:52 +0000

    optimize register/login process, update html extended base file, added char page via logout btn

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template, redirect, url_for, session
+from flask import Flask, request, jsonify, render_template, redirect, url_for,flash, session
 from models import db, User
 
 
@@ -24,48 +24,41 @@ db.init_app(app)
 # Реєстрація користувача
 @app.route('/register', methods=['POST'])
 def register():
-    data = request.json
-    username = data.get('username')
-    password = data.get('password')
-
-    if not username or not password:
+    username = request.form['regUsername']
+    password = request.form['regPassword']
+    if not username or not password:  # TODO не використовуємо цю перевірку
         return jsonify({'message': 'Введіть логін та пароль'}), 400
 
-    conn = sqlite3.connect(db_path)
-    cursor = conn.cursor()
-    try:
-        cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
-        conn.commit()
-    except sqlite3.IntegrityError:
-        return jsonify({'message': 'Користувач з таким ім’ям уже існує'}), 400
-    finally:
-        conn.close()
-
-    return jsonify({'message': 'Реєстрація пройшла успішно'}), 201
+    existing_user = User.query.filter_by(username=username).first()
+    if existing_user:
+        flash({'message': 'Користувач з таким ім’ям уже існує'}), 400  #TODO add notification aboust success on invalid creds
+        return render_template('index.html')
+    
+    new_user = User(username=username, password=password)
+    db.session.add(new_user)
+    db.session.commit()
+    flash('Реєстрація пройшла успішно', 201)
+    return render_template('index.html')
 
 # Авторизація користувача
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-       data = request.json
-       username = data.get('username')
-       password = data.get('password')
+   if request.method == 'POST':
+        username = request.form['loginUsername']
+        password = request.form['loginPassword']
+        # Перевірка існування користувача в базі даних через SQLAlchemy
+        user = User.query.filter_by(username=username, password=password).first()
+        
+        if user:
+            # Зберігаємо user_id у сесії після успішної авторизації
+            session['user_id'] = user.id
+            return redirect(url_for('character'))
+        
+        # Повертаємо помилку, якщо логін або пароль неправильні
+        flash({'message': 'Невірний логін або пароль'}), 401
+        return render_template('index.html')
 
-       conn = sqlite3.connect(db_path)
-       cursor = conn.cursor()
-       cursor.execute("SELECT * FROM users WHERE username = ? AND password = ?", (username, password))
-       user = cursor.fetchone()
-       conn.close()
-
-       if user:
-           # Зберігаємо ідентифікатор користувача в сесії
-        #    session['user_id'] = user.id
-           # Перенаправляємо на сторінку персонажа
-           return redirect(url_for('character'))
-       else:
-           return jsonify({'message': 'Невірний логін або пароль'}), 401
-       
-    return render_template('index.html')
+   return render_template('index.html')
 
 @app.route('/logout')
 def logout():
@@ -75,19 +68,17 @@ def logout():
 
 @app.route('/character')
 def character():
-    # Перевіряємо, чи користувач залогінений
-    if 'user_id' not in session:
-        return redirect(url_for('login'))
-    
     user_id = session['user_id']
-    user = User.query.get(user_id)
-    
-    if not user:
+    if not user_id:
         return redirect(url_for('login'))
-    
-    return render_template('character.html', user=user)
+    user = User.query.get(user_id)  # Використовуємо SQLAlchemy для отримання користувача за ID
+    if user:
+        return render_template('character.html', user=user)
+    else:
+        return redirect(url_for('login'))  # Якщо користувача немає, переходимо на сторінку входу
 
 @app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html')
 
