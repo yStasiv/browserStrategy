65c1cb632194e3c6b5222cacfed269758463af1c
yStasiv <yurastasiv@gmail.com>
2025-02-20 01:01:10 +0200

    Add enterprises logic

--- backend/helpers/calc.py
+++
@@ -1,9 +0,0 @@
-"""File containe calcs for game mechanics"""
-class CalsculateExpForNextLvl():
-
-    def experience_needed_for_next_level(level: int) -> int:
-        """Обчислює необхідний досвід для досягнення наступного рівня."""
-        if level >= 8:
-            return None  # Гравець досяг максимального рівня
-        base_experience = 1000
-        return int(base_experience * (1.3 ** (level - 1)))
